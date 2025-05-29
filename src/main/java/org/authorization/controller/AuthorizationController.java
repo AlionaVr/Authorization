eb8a5cfbac453f559f71d570b9ca68c06ba8d214
AlionaVr <alonavershilo@yandex.by>
2025-05-29 20:33:01 +0300

    add ExceptionHandlerAdvice

--- src/main/java/org/authorization/controller/AuthorizationController.java
+++ src/main/java/org/authorization/controller/AuthorizationController.java
@@ -2,12 +2,7 @@ package org.authorization.controller;
 
 import lombok.AllArgsConstructor;
 import org.authorization.Authorities;
-import org.authorization.exception.InvalidCredentials;
-import org.authorization.exception.UnauthorizedUser;
 import org.authorization.service.AuthorizationService;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,15 +19,4 @@ public class AuthorizationController {
                                             @RequestParam("password") String password) {
         return service.getAuthorities(user, password);
     }
-
-    @ExceptionHandler(InvalidCredentials.class)
-    public ResponseEntity<String> InvalidCredentialsHandler(InvalidCredentials e) {
-        return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
-    }
-
-    @ExceptionHandler(UnauthorizedUser.class)
-    public ResponseEntity<String> UnauthorizedUserHandler(UnauthorizedUser e) {
-        return new ResponseEntity<>(e.getMessage(), HttpStatus.UNAUTHORIZED);
-    }
-
 }
